03a000e80756abe7770273e821282906a67ddec7
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-07-10 13:49:24 +0530

    list

---
+++ core/data-collection/0-list.py
@@ -0,0 +1,100 @@
+"""
+List : mutable, odered, duplicate values are allowed, indexing, slicing
+
+syntax :
+list_name = [value1, value2, value3, ...]
+list_name = list([value1, value2, value3, ...])
+list_name = list(range(start, end, step))
+list_name = list(range(start, end))
+list_name = list(range(end))
+
+empty_list = []
+empty_list = list()
+"""
+
+nums = [1,2,3,4,5]
+
+# print(type(nums))
+# print(len(nums))
+
+mummy_list = ['milk', 'potato']
+aunty_list = ['butter-milk', 'potato']
+my_list = ['drink', 'chocolate']
+sis_list = ['ice-cream']
+
+# concate tow or more lists
+# mix_list = mummy_list + aunty_list + my_list + sis_list 
+
+# print(mix_list)
+# mix_list[-1] = 'book'
+# print(mix_list)
+
+# print(my_list * 2) # replica
+
+# nums = [1,2,3,4,5]
+
+# print(nums[0])
+
+# nums[nums[0]] = 3
+
+# print(nums)
+
+# mix_data = [1, 3.4, 'dshgfd', 34 + 36j]
+# print(mix_data)
+
+lis = []
+
+# print(dir(lis))
+
+# nums = [1,2,3,4].append(5)
+# print(nums)
+
+# nums = [1,2,3,4,5,1,21,1,1,1,1]
+# nums.sort(reverse=True)
+# print(nums)
+
+# print(nums.count(1))
+
+# print(nums.index(3))
+# print(nums.index(1, 2))
+# print(nums)
+
+# add
+# new_nums = [6,7,8]
+# nums.append(new_nums)
+# nums.extend(new_nums)
+# nums += new_nums
+# nums.insert(0, new_nums)
+# nums.insert(2, 6)
+
+# delete
+# nums.clear()
+# del nums[0]
+# nums.pop(-2)
+# nums.remove(3)
+
+# copy_list = nums.copy()
+# print(id(copy_list))
+# print(id(nums))
+
+# x = 10
+# y = 10
+
+# print(id(x))
+# print(id(y))
+
+# users = []
+# for num in range(2):
+#     user = []
+#     user.insert(0, input("Enter your name : "))
+#     users.append(user)
+
+# print(users)
+
+
+# users = []
+# for num in range(2):
+#     users.append(input("Enter your name : "))
+
+# print(users)
+
---
+++ ip/list.py
@@ -0,0 +1,9 @@
+items = ['milk', 'chocolate', 'monky', 'dudhi', 'god', 'pani']
+
+# [
+#     ['milk', 4],
+#     ['chocolate', 9],
+#     .,
+#     .,
+#     ['pani', 4]
+# ]
