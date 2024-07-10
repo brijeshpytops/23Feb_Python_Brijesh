e7dba9d7e206dcf3f3ad6fcc2b5d9c21e6f03cdf
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-07-10 14:21:38 +0530

    ip

--- ip/list.py
+++ ip/list.py
@@ -1,4 +1,4 @@
-items = ['milk', 'chocolate', 'monky', 'dudhi', 'god', 'pani']
+words = ['milk', 'chocolate', 'monky', 'dudhi', 'god', 'pani']
 
 # [
 #     ['milk', 4],
@@ -7,3 +7,19 @@ items = ['milk', 'chocolate', 'monky', 'dudhi', 'god', 'pani']
 #     .,
 #     ['pani', 4]
 # ]
+
+# [
+#     ['milk', 4, vowels, consonent],
+#     ['chocolate', 9, , vowels, consonent],
+#     .,
+#     .,
+#     ['pani', 4, , vowels, consonent]
+# ]
+
+words = input("Enter a string: ").split(" ") 
+
+for index, word in enumerate(words):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    words[index] = [word, len(word), len([ch for ch in word if ch in vowels]), len(word) - len([ch for ch in word if ch in vowels])]
+
+print(words)
