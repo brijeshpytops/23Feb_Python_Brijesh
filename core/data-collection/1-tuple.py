800d6e92a4900f4725d7dbf2f9054485eb79637e
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-07-12 13:41:42 +0530

    tuple, set and frozenset

---
+++ core/data-collection/1-tuple.py
@@ -0,0 +1,30 @@
+"""
+Tuple : immutable, ordered, duplicate values are allowed, slicing, indexing
+
+suntax : 
+tuple_name = tuple()
+tuple_name = (value1, value2, value3, ...)
+tuple_name = (value1, value2, value3, ..., valueN)
+tuple_name = ()
+comma_tuple = 1,
+"""
+
+nums = 1,2,3,4,5
+# print(type(nums))
+# print(len(nums))
+
+# print(nums[0])
+# print(nums[1:3])
+# print(nums)
+
+# nums[1] = 22
+
+# print(nums) # TypeError: 'tuple' object does not support item assignment
+
+# print(dir(nums))
+
+# tokens = tuple([1,2,3,4,5])
+# # print(tokens[0])
+
+# tokens[0] = 1
+# print(tokens)
---
+++ core/data-collection/2-set.py
@@ -0,0 +1,29 @@
+"""
+set] mutable/immutable, unordered, duplicate values are not allowed, you can not indexing and slicing.
+set is a collection of unique elements. It is an unordered collection of items. It is iterable.
+set is mutable, but it is not indexable. It is not subscriptable. It is not
+sliced.
+
+set_name = set()
+set_name = {1,2,3,4}
+"""
+# nums = {1,2,1,2,1,2,12,1,2,3,23,23,1,53,323,21,2,12,}
+# # print(type(nums))
+# print(nums)
+
+# Creating a set with some elements
+# my_set = {3, 1, 4, 2, 5}
+
+# # Printing the set multiple times
+# print(my_set)  # Output might be {1, 2, 3, 4, 5}
+# print(my_set)  # Output might be {2, 4, 1, 3, 5}
+
+# As we can see, the order of elements is not guaranteed to be the same every time
+
+# my_set.add(11)
+# my_set[0] = 11
+
+# my_set = frozenset(my_set)
+# my_set.add(11)
+
+# print(my_set)
