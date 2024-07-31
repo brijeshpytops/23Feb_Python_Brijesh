1f52d9758736ef74cce3373a15e0d9c400d1dced
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-07-31 15:12:13 +0530

    file handling

---
+++ advance/file_handling/main.py
@@ -0,0 +1,46 @@
+file_name = 'sample.txt'
+
+# open(file_name, 'x')
+# file = open(file_name, 'r')
+# print(file.tell())
+# file.seek(5)
+# print(file.tell())
+# name = "Python for everybody"
+# file.write(name)
+
+# print(file.read(4))
+# print(file.read())
+
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+
+# print(file.readlines())
+
+# lines = ['This si a line - 1\n', 'This si a line - 2\n', 'This si a line - 3\n', 'This si a line - 4\n', 'This si a line - 5\n', 'This is a new line']
+# file.writelines(lines)
+
+# file.write('\nthis is line - 8')
+# file.close()
+
+# print(file.read())
+
+
+import os
+import uuid
+
+# old_file_name = 'd8e97008-ea85-4c2b-8d1d-eee05044dab2.txt'
+# os.system('type nul > smaple1.txt')
+
+# os.rename(old_file_name, f"{uuid.uuid4()}.txt")
+
+# os.remove(old_file_name)
+
+# f = open('logo.png', 'rb')
+# print(f.read())
+
+# print(dir(os))
